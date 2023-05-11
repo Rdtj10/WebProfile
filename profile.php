47d76e4d38c4398de5e54c3feb34758eaace0f90
Rdtj10 <106721821+Rdtj10@users.noreply.github.com>
2023-05-11 12:52:55 +0700

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, shrink-to-fit=no, user-scalable=no">
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+	</head>
+	<body>
+		<h1>Profile Site</h1>
+		<p id="desc"><em>I'm Ridho Dimas, One of Bangkit Cohort!</em></p>
+		<img id="profile-img"src="profile.jpg" alt="profile logo" title="profile">
+		<p id="tagline"><i>Find me on: </i></p>
+		<div id="profile-social-icons">
+	    	<a href="" target="_blank"><i class="fab fa-github animated fadeIn" aria-hidden="true"></i></a>
+	    	<a href="" target="_blank"><i class="fab fa-facebook animated fadeIn" aria-hidden="true"></i></a>
+			<a href="" target="_blank"><i class="fab fa-instagram animated fadeIn" aria-hidden="true"></i></a>
+	    	<a href="" target="_blank"><i class="fab fa-linkedin animated fadeIn" aria-hidden="true"></i></a>
+	    </div>
+		<br>
+
+		<div class="container">
+			<div class="content">
+				<h2>Skills</h2>	
+				<ul>
+					<li>Back end Programming seperti PHP,Python(flask),HTML</li>
+					<li>Front end Programming seperti CSS, Javascript</li>
+					<li>Perancangan database dengan MySQL</li>
+					
+				</ul>
+				<h2>Experience</h2>	
+				<p>Saya telah merancang sebuah website untuk sistem informasi Kecamatan. Pembuatan Web dibuat dengan bahasa Python dengan library flask sebagai backend, bootstrap(css) sebagai front end dan API yang digunakan adalah MySQL. Website ini sendiri masih dalam tahap pengembangan,karena ini adalah projek uji coba pertama saya menggunakan python sebagai back end. Dan juga saya telah membuat website untuk reservasi hotel dalam memenuhi tugas akhir saya.Website ini merupakan website yang bertujuan untuk mempermudah pegawai hotel dalam mengelola kamar hotel, dan menginput data tamu hotel. Website dibuat dengan bahasa PHP,HTML, dan CSS</p>
+			</div>
+			<div class="content">
+				<h2>About me!</h2>
+				<div class="list"> Name           : <b>Ridho Dimas Tri Prasetyo Jayadi</b></div>
+				<div class="list"> Age            : <b>21</b></div>
+				<div class="list"> Date of Birth  : <b>10th April, 2002</b></div>
+				<div class="list"> Place of Birth : <b>Tualang</b></div>
+				<div class="list"> Hobbies : <b>Playing Basketball, Fishing, Programming, Gaming</b></div>
+				<div class="list"> College : <b>Universitas Negeri Padang</b></div>
+				<br>
+				<p>Saya merupakan mahasiswa informatika yang memiliki ketertarikan terhadap teknologi komputer seperti perancangan website. Ketertarikan saya pada teknologi komputer menjadi motivasi tinggi bagi saya untuk mengikuti pelatihan Website Development. Berbekal basic back end dan front end programming, saya mencari kesempatan untuk mengembangkan skill saya dalam bidang ini.</p>
+			</div>
+		</div>
+
+		<footer>
+			<p>Ridho Dimas, &copy;Copyright 2023</p>
+		</footer>
+	</body>
+</html>
+
+	
+	
